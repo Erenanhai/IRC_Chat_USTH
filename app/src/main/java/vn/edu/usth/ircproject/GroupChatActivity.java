524b7f2750dcff2150ba81c2af1337fa4af3b094
damlong1 <damthanlong@gmail.com>
2023-01-11 13:39:12 +0700

    Create group chat layout

---
+++ app/src/main/java/vn/edu/usth/ircproject/GroupChatActivity.java
@@ -0,0 +1,39 @@
+package vn.edu.usth.ircproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ScrollView;
+import android.widget.TextView;
+import androidx.appcompat.widget.Toolbar;
+
+public class GroupChatActivity extends AppCompatActivity {
+
+    private Toolbar mToolbar;
+    private ImageButton SendMessageButton;
+    private EditText userMessageInput;
+    private ScrollView mScrollView;
+    private TextView displayTextMessages;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_goup_chat);
+
+        InitializeFields();
+    }
+
+    private void InitializeFields() {
+        mToolbar = (Toolbar) findViewById(R.id.group_chat_bar_layout);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setTitle("Group Name");
+
+        SendMessageButton = (ImageButton) findViewById(R.id.send_message_button);
+        userMessageInput = (EditText) findViewById(R.id.input_group_message);
+        displayTextMessages = (TextView) findViewById(R.id.group_chat_display);
+        mScrollView = (ScrollView) findViewById(R.id.my_scroll_view);
+
+    }
+}
